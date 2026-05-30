e494c22d299eef541752d88868aff890e7ae04ab
kodurivasishta <kodurivasishta@gmail.com>
2026-05-30 12:34:16 +0530

    adding ai.classifier and duplicate detector

---
+++ test.py
@@ -0,0 +1,5 @@
+from utils.ai_classifier import classify_complaint
+
+print(classify_complaint("No water supply for 2 days"))
+print(classify_complaint("Huge pothole on road"))
+print(classify_complaint("Power cut since morning"))
---
+++ utils/duplicate_detector.py
@@ -0,0 +1,16 @@
+from difflib import SequenceMatcher
+
+def is_duplicate(new_complaint, existing_complaints, threshold=0.8):
+
+    for complaint in existing_complaints:
+
+        similarity = SequenceMatcher(
+            None,
+            new_complaint.lower(),
+            complaint.lower()
+        ).ratio()
+
+        if similarity > threshold:
+            return True
+
+    return False
