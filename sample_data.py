d6d0b8bfc47fb9c6c87052f516161d2baeebaf97
Dhruva Gadegoni <dhruvagadegoni@gmail.com>
2026-05-30 14:45:06 +0530

    adding database.py and complaint.db

---
+++ sample_data.py
@@ -0,0 +1,26 @@
+from utils.database import *
+
+create_table()
+
+add_complaint(
+    "Road damaged near bus stop",
+    "Road",
+    "Hyderabad",
+    "2026-05-30"
+)
+
+add_complaint(
+    "Power outage in colony",
+    "Electricity",
+    "Hyderabad",
+    "2026-05-30"
+)
+
+add_complaint(
+    "Garbage not collected",
+    "Garbage",
+    "Hyderabad",
+    "2026-05-30"
+)
+
+print(get_all_complaints())
---
+++ test_database.py
@@ -0,0 +1,12 @@
+from utils.database import *
+
+create_table()
+
+add_complaint(
+    "Water supply issue",
+    "Water",
+    "Hyderabad",
+    "2026-05-30"
+)
+
+print(get_all_complaints())
